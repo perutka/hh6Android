7ef736541797531ee284a94575d51c792c2951f8
primož perušek <primozperusek@gmail.com>
2018-08-01 09:50:26 +0200

    first commit

---
+++ app/src/test/java/com/android/hh6/healhero6android/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.android.hh6.healhero6android;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
